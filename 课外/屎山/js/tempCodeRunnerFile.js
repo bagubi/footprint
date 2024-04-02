cf5f63ae7a3efd32314ad3f017f8d451ba8152b8
bagubi <1975307686@qq.com>
2024-04-02 17:11:55 +0800

    233

--- 课外/屎山/js/tempCodeRunnerFile.js
+++ 课外/屎山/js/tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-console.log(arr);
+let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let res2 = ar.pop();
+console.log(res2);//删除的元素
+console.log(ar);
--- 课外/屎山/js/数组新增.js
+++
@@ -1,9 +0,0 @@
-// 在后加
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let res = arr.push('好悬没给我李宁踹开线');
-console.log(res);//数组新增后的长度
-console.log(arr);
-// 前加
-let res1 = arr.unshift('好悬没给我李宁踹开线');
-console.log(res1);//数组新增后的长度
-console.log(arr);
