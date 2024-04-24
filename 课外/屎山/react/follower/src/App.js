afdde1d0114dec95e17a187968516cedad293cd5
bagubi <1975307686@qq.com>
2024-04-24 21:51:27 +0800

    发生了什么

--- 课外/屎山/react/follower/src/App.js
+++ 课外/屎山/react/follower/src/App.js
@@ -1,8 +1,16 @@
+const list = [{ id: 1001, name: 'vue' },
+{ id: 1002, name: 'vue' }]
+
+
+
 
 function App() {
   return (
     <div className="App">
       。。。
+      <ul>
+        {list.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
