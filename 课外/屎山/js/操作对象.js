04f29c77a20c3e0fa37351b721b489cc2ac4f6ce
bagubi <1975307686@qq.com>
2024-10-20 20:53:17 +0800

    ..

--- 课外/屎山/js/操作对象.js
+++ 课外/屎山/js/操作对象.js
@@ -1,15 +1,46 @@
 let ob = {
-    'pig-name': 'why',
-    age: 18
-}
+  "pig-name": "why",
+  age: 18,
+};
 
 // 获取属性值
-console.log(ob['pig-name']);
+console.log(ob["pig-name"]);
 
 // 直接访问对象属性age（无需变量）
-console.log(ob['age']); // 或者 console.log(ob.age);
+console.log(ob["age"]); // 或者 console.log(ob.age);
 
-let str = 'age';
+let str = "age";
 
 // 使用变量str作为属性名访问对象属性
 console.log(ob[str]);
+
+// 操作对象：对数据 查、增、改、删
+let pig = {
+  uname: "佩奇",
+  sing: function () {
+    console.log("唱歌");
+  },
+};
+
+// 1. 查： 对象.属性 对象.方法
+console.log(pig.uname); // 得到属性值
+pig.sing();
+
+// 2. 增：对象.新属性 = 新值   对象.新方法 = function(){}
+pig.age = 4;
+pig.dance = function () {
+  console.log("跳舞");
+};
+console.log(pig);
+
+// 3. 改：对象.属性 = 新值  对象.方法 = 新匿名函数
+pig.uname = "小猪佩奇";
+pig.sing = function () {
+  console.log("哼哼哼！！");
+};
+console.log(pig);
+
+// 4. 删： 了解，因为我们很少对对象里面的数据做删除操作  delete
+delete pig.age;
+delete pig.dance;
+console.log(pig);
