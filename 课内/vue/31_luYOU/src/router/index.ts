93f2e8d9c89b102d7afb47d327934ba924ede4a7
Bagubi <1975307686@qq.com>
2026-08-18 16:44:37 +0800

    33

--- 课内/vue/31_luYOU/src/router/index.ts
+++ 课内/vue/31_luYOU/src/router/index.ts
@@ -1,8 +1,14 @@
-//这是个路由组件
+//主路由配置文件（必须）
 //创建一个路由器并暴露出去
 
 // 第一步：引入createRouter：创建路由器
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+//createWebHistory：history模式
+//createWebHashHistory：hash模式
 
 //引入一个一个可能要呈现的组件
 import Home from "@/pages/Home.vue";
@@ -12,7 +18,8 @@ import News from "@/pages/News.vue";
 // 第二步：创建路由器对象
 const router = createRouter({
   //   router:路由器（一个应用有一个路由器就够了，用来管理路由）
-  history: createWebHistory(), //路由器工作模式：hash模式、history模式
+  // history: createWebHistory(), //路由器工作模式：history模式
+  history: createWebHashHistory(), //路由器工作模式：hash模式(模式的转变看着行)
   routes: [
     //routes里是一个一个的路由规则
 
